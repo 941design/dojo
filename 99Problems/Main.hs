46d200632fb60ca97f59d8008ff1991463dedeb8
Markus Rother <markus.rother@qudosoft.de>
2022-01-28 16:33:36 +0100

    99Problems/14-22 initial commit

--- 99Problems/Main.hs
+++ 99Problems/Main.hs
@@ -83,6 +83,10 @@ packl :: Eq a => [a] -> [(Int, a)]
 packl xs = [(len xs, head xs) | xs <- pack xs]
 
 
+-- | P11 (*) Modified run-length encoding.
+-- Not applicable, due to type constraints (unless using Either)
+
+
 -- | P12 (**) Decode a run-length encoded list.
 unpackl :: [(Int, a)] -> [a]
 unpackl [] = []
@@ -102,6 +106,63 @@ packl2 (x:xs) = acc ++ [(i, c)]
     fn (i, c, acc) x
       | c == x = (i+1, c, acc)
       | otherwise = (1, x, acc ++ [(i, c)])
+
+
+-- | P14 (*) Duplicate the elements of a list.
+dupl :: [a] -> [a]
+dupl [] = []
+dupl (x:xs) = [x, x] ++ dupl xs
+
+
+-- | P15 (**) Duplicate the elements of a list a given number of times.
+dupln :: Int -> [a] -> [a]
+dupln _ [] = []
+dupln n (x:xs) = (take n $ repeat x) ++ dupln n xs
+
+
+-- | P16 (**) Drop every Nth element from a list.
+dropnth :: Int -> [a] -> [a]
+dropnth _ [] = []
+dropnth n xs = [x | (i, x) <- zip [1..] xs, i `mod` n /= 0]
+
+
+-- | P17 (*) Split a list into two parts.
+splitn :: Int -> [a] -> ([a], [a])
+splitn n xs = (take n xs, drop n xs)
+
+
+-- | P18 (**) Extract a slice from a list.
+slice :: Int -> Int -> [a] -> [a]
+slice n k xs = take (k-n) $ drop n $ xs
+
+
+-- | P19 (**) Rotate a list N places to the left.
+rotate :: Int -> [a] -> [a]
+rotate 0 xs = xs
+rotate n xs
+  | n > 0 = drop n xs ++ take n xs
+  | otherwise = rotate ((l+n) `mod` l) xs
+  where l = len xs
+
+
+-- | P20 (*) Remove the Kth element from a list.
+remn :: Int -> [a] -> [a]
+remn _ [] = []  -- no error
+remn 0 (x:xs) = xs
+remn n (x:xs) = x:remn (n-1) xs
+
+
+-- | P21 (*) Insert an element at a given position into a list.
+insn :: a -> Int -> [a] -> [a]
+insn a 0 [] = [a]
+insn _ _ [] = [] -- no error
+insn a 0 xs = a:xs
+insn a n (x:xs) = x:insn a (n-1) xs
+
+
+-- | P22 (*) Create a list containing all integers within a given range.
+irange :: Int -> Int -> [Int]
+irange n k = slice n k [0..]
 
 
 -- | P49 (**) Gray code.
