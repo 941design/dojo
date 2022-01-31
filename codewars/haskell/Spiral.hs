de1f4a2d76c1cb82e3ed8f52542ca0cca296f0e0
Markus Rother <markus.rother@qudosoft.de>
2022-01-31 23:59:50 +0100

    codewars/haskell/Spiral initial commit

---
+++ codewars/haskell/Spiral.hs
@@ -0,0 +1,28 @@
+module Spiral where
+
+
+-- | https://www.codewars.com/kata/534e01fbbb17187c7e0000c6
+spiralize :: Int -> [[Int]]
+spiralize n = [[ if elem (x,y) path then 1 else 0 | y <- [0..n-1]] | x <- [0..n-1]]
+  where
+    -- vectors are replicated to create a spiral path
+    -- vectors ::= [right, down, left, up, ...]
+    vecs = cycle [(0,1), (1,0), (0,-1), (-1,0)]
+    -- repetitions from outside to the inside, starting with one full line,
+    -- then repeating two lines of the same length,
+    -- finally we need to distinguish squares of even width from uneven for the last line of length 1 if any
+    reps = [n-1] ++ concatMap (replicate 2) [n-1, n-3..2] ++ if even n then [1] else []
+    -- creating path by repeatedly applying vectors to coordinates starting at (0,0) i.e. top left
+    path = scanl add (0,0) $ concatMap (uncurry replicate) $ zip reps vecs
+      where add (x,y) (v,w) = (x+v, y+w)
+
+
+-- | inside out
+spiral :: Int -> [String]
+spiral 1 = ["0"]
+spiral n = extend . rotate $ spiral (n-1)
+  where rotate :: [[a]] -> [[a]]
+        rotate m = [[m !! i !! j | i <- reverse [0..length m - 1]] | j <- [0..length m - 1]]
+        extend :: [String] -> [String]
+        extend m = replicate (length m + 1) '0' : map ((:) '.') m
+
