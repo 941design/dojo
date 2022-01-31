26d6c7fd7ee3aca6b9eff69c0fb9084114d33c85
Markus Rother <markus.rother@qudosoft.de>
2022-02-01 00:05:17 +0100

    codewars/haskell/Spiral added comments

--- codewars/haskell/Spiral.hs
+++ codewars/haskell/Spiral.hs
@@ -2,15 +2,19 @@ module Spiral where
 
 
 -- | https://www.codewars.com/kata/534e01fbbb17187c7e0000c6
+-- create a spiral path in a square
+--
+-- learnings::
+-- Found a very concise solution after completion, which required matrix rotation.
+-- I had thought about it, but it didn't occur to me that reversal and transposition is sufficient.
 spiralize :: Int -> [[Int]]
 spiralize n = [[ if elem (x,y) path then 1 else 0 | y <- [0..n-1]] | x <- [0..n-1]]
   where
-    -- vectors are replicated to create a spiral path
     -- vectors ::= [right, down, left, up, ...]
     vecs = cycle [(0,1), (1,0), (0,-1), (-1,0)]
-    -- repetitions from outside to the inside, starting with one full line,
-    -- then repeating two lines of the same length,
-    -- finally we need to distinguish squares of even width from uneven for the last line of length 1 if any
+    -- path lengths from outside to the inside, starting with one full line,
+    -- then repeating two lines of the same length, until reaching the spiral's center.
+    -- finally we need to distinguish squares of even width from uneven for the last line of length 1 if any.
     reps = [n-1] ++ concatMap (replicate 2) [n-1, n-3..2] ++ if even n then [1] else []
     -- creating path by repeatedly applying vectors to coordinates starting at (0,0) i.e. top left
     path = scanl add (0,0) $ concatMap (uncurry replicate) $ zip reps vecs
@@ -18,6 +22,7 @@ spiralize n = [[ if elem (x,y) path then 1 else 0 | y <- [0..n-1]] | x <- [0..n-
 
 
 -- | inside out
+-- this yields a different result and does not meet the requirement above that we always start with one full line.
 spiral :: Int -> [String]
 spiral 1 = ["0"]
 spiral n = extend . rotate $ spiral (n-1)
